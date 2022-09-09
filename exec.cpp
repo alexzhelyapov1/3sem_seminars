6dc71566fe468dc31e5d1e02d0f27a2b5eed92ae
liza_2.0 <55490812+alexzhelyapov1@users.noreply.github.com>
2022-09-09 18:22:41 +0300

    Fork with context substitution and counter
    ./a.out /bin/ls -la /home-local/student/Desktop/temp_directory_Alex_Zhelyapov/2_seminar
    ./a.out /bin/ls -la /

--- exec.cpp
+++ exec.cpp
@@ -2,6 +2,13 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[]) {
-    execvp (argv[1], argv + 1);
+    int pid = fork();
+	if (pid < 0)
+		printf ("Proccess didn't create(\n");
+	else if (pid == 0) 
+        execvp (argv[1], argv + 1);
+	else 
+        for (int i = 0; i < 1200; i++) printf ("%d\n", i);
+
     return 0;
 }
